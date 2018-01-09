faf8c517b5dae9d2fa3bba01853e24382f788a77
Aart Bik <ajcbik@google.com>
2018-01-09 11:01:02 -0800

    Improve code sinking near "always throwing" method calls
    Rationale:
    With simple dex bytecode analysis, the inliner marks methods
    that always throw to help subsequent code sinking. This reduces
    overhead of non-nullable enforcing calls found in e.g the Kotlin
    runtime library (1%-2% improvement on tree microbenchmark, about
    5% on Denis' benchmark).
    
    Test: test-art-host test-art-target
    
    Change-Id: I45348f049721476828eb5443738021720d2857c0
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- compiler/optimizing/optimizing_compiler_stats.h
+++ compiler/optimizing/optimizing_compiler_stats.h
@@ -74,6 +74,7 @@ enum class MethodCompilationStat {
   kImplicitNullCheckGenerated,
   kExplicitNullCheckGenerated,
   kSimplifyIf,
+  kSimplifyThrowingInvoke,
   kInstructionSunk,
   kNotInlinedUnresolvedEntrypoint,
   kNotInlinedDexCache,
