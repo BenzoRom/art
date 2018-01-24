dd41bacce6b5c617ee847a0b26219091f23a7bea
Denis Hsu <denis.hsu@mediatek.com>
2018-01-24 19:52:30 +0800

    Fix CC garbage collection deadlock
    Set GC critical section to prevent HeapTaskDaemon to do barrier
    operation and disable weak_ref_access_enabled while signal catcher
    dump class linker.
    
    This is a deadlock with 3 threads causes:
    	1. HeapTaskDaemon
    	2. Signal Catcher
    	3. IpManager.wlan0
    
    Threads block relationship
    HeapTaskDaemon  -> IpManager.wlan0 -> Signal Catcher
                ↑_________________________________↓
    
    HeapTaskDaemon
    	1. #3 RevokeThreadLocalMarkStacks
    	2. Request check point
    	3. Set weak_ref_access_enabled false
    	4. Wait IpManager.wlan0 barrier pass
    
    HeapTaskDaemon sysTid=14293, pid=14272
     #0 pc 7f63c775f0   /system/lib64/libc.so syscall /proc/self/cwd/bionic
     /libc/arch-arm64/bionic/syscall.S:41
     #1 pc 7eded14640   /system/lib64/libart.so art::futex(int volatile*,
     int, int, timespec const*, int volatile*, int) art/runtime/base/
     mutex-inl.h:43 art::ConditionVariable::WaitHoldingLocks(art::Thread*)
     art/runtime/base/mutex.cc:931
     #2 pc 7eded0de3c   /system/lib64/libart.so art::Barrier::Increment(
     art::Thread*, int) art/runtime/barrier.cc:58
     #3 pc 7edee14a38   /system/lib64/libart.so art::gc::collector::
     ConcurrentCopying::RevokeThreadLocalMarkStacks(bool, art::Closure*)
     art/runtime/gc/collector/concurrent_copying.cc:1374
    
    IpManager.wlan0
    	1. #2 Wait Locks::classlinker_classes_lock_
    
    IpManager.wlan0 sysTid=14439, pid=14272
     #0 pc 7f63c775f0   /system/lib64/libc.so syscall /proc/self/cwd/bionic
     /libc/arch-arm64/bionic/syscall.S:41
     #1 pc 7eded1367c   /system/lib64/libart.so art::futex(int volatile*,
     int, int, timespec const*, int volatile*, int) art/runtime/base/
     mutex-inl.h:43 art::ReaderWriterMutex::ExclusiveLock(art::Thread*)
     art/runtime/base/mutex.cc:629
     #2 pc 7eded5b048   /system/lib64/libart.so WriterMutexLock art/runtime
     /base/mutex.h:529
    art::ClassLinker::FindClass(art::Thread*, char const*, art::Handle<
    art::mirror::ClassLoader>) art/runtime/class_linker.cc:2793
    
    Signal Catcher 
    	1. Held Locks::classlinker_classes_lock_
    	2. #2 Wait weak_ref_access_enabled true
    
    Signal Catcher sysTid=14279, pid=14272
     #0 pc 7f63c775f0   /system/lib64/libc.so syscall /proc/self/cwd/bionic
     /libc/arch-arm64/bionic/syscall.S:41
     #1 pc 7eded14640   /system/lib64/libart.so art::futex(int volatile*,
     int, int, timespec const*, int volatile*, int) art/runtime/base/
     mutex-inl.h:43 art::ConditionVariable::WaitHoldingLocks(art::Thread*)
     art/runtime/base/mutex.cc:931
     #2 pc 7edef2d9d0   /system/lib64/libart.so art::JavaVMExt::
     DecodeWeakGlobalLocked(art::Thread*, void*) art/runtime/
     java_vm_ext.cc:741
    
    Bug: 72457759
    Change-Id: Ia8a0712c649b4099f0b51cc41610eebd32c25d17
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- runtime/class_linker.cc
+++ runtime/class_linker.cc
@@ -8506,6 +8506,8 @@ void ClassLinker::SetEntryPointsForObsoleteMethod(ArtMethod* method) const {
 
 void ClassLinker::DumpForSigQuit(std::ostream& os) {
   ScopedObjectAccess soa(Thread::Current());
+  gc::ScopedGCCriticalSection gcs(Thread::Current(), gc::kGcCauseClassLinker,
+                                  gc::kCollectorTypeClassLinker);
   ReaderMutexLock mu(soa.Self(), *Locks::classlinker_classes_lock_);
   os << "Zygote loaded classes=" << NumZygoteClasses() << " post zygote classes="
      << NumNonZygoteClasses() << "\n";
