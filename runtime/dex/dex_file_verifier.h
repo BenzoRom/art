d70854ddb05b2ca04c1be341752e5e5980bf69cb
Vladimir Marko <vmarko@google.com>
2018-05-08 18:11:25 +0100

    ART: Templatize DexFileVerifier::CheckIntraIdSection()
    And use macros in DexFileVerifier::CheckIntraSection() to
    avoid code duplication.
    
    This is a follow-up to
        https://android-review.googlesource.com/672438
    but the improvements in instruction count measured on host
    with "perf stat dexdump2 -c <large apk>" are absolutely
    insignificant (0.0003%).
    
    Test: Rely on TreeHugger.
    Change-Id: I95fd3b2f7f2d767e5446dd71552b34ea55f4bd47
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- runtime/dex/dex_file_verifier.h
+++ runtime/dex/dex_file_verifier.h
@@ -127,8 +127,8 @@ class DexFileVerifier {
 
   template <DexFile::MapItemType kType>
   bool CheckIntraSectionIterate(size_t offset, uint32_t count);
-  bool CheckIntraSectionIterateByType(size_t offset, uint32_t count, DexFile::MapItemType type);
-  bool CheckIntraIdSection(size_t offset, uint32_t count, DexFile::MapItemType type);
+  template <DexFile::MapItemType kType>
+  bool CheckIntraIdSection(size_t offset, uint32_t count);
   template <DexFile::MapItemType kType>
   bool CheckIntraDataSection(size_t offset, uint32_t count);
   bool CheckIntraSection();
